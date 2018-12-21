f7b5512a6aeb10d39b2162993c2bdb89541c71bb
Marisa DeMeglio <marisa.demeglio@gmail.com>
2018-12-21 00:02:04 -0800

    About box has version and logo
    Fixes #8

--- src/main/menu.js
+++ src/main/menu.js
@@ -7,6 +7,7 @@ import {
 } from './../shared/actions/app';
 import {selectTab} from './../shared/actions/reportView';
 import * as FileDialogHelpers from './../shared/helpers/fileDialogs';
+import * as AboutBoxHelper from './../shared/helpers/about';
 export default class MenuBuilder {
 
   constructor(mainWindow, store) {
@@ -201,7 +202,9 @@ export default class MenuBuilder {
         submenu: [
           {
             label: 'About Ace',
-            click: () => dialog.showMessageBox({"message": "Ace Beta", "detail": "DAISY Consortium 2018"})
+            id: 'about',
+            click: () => AboutBoxHelper.showAbout()
+            //click: () => dialog.showMessageBox({"message": "Ace Beta", "detail": "DAISY Consortium 2018"})
           },
           {
             type: 'separator'
@@ -270,7 +273,9 @@ export default class MenuBuilder {
         },
         {
           label: 'About Ace',
-          click: () => dialog.showMessageBox({"message": "Ace Beta", "detail": "DAISY Consortium 2018"})
+          id: 'about',
+          click: () => AboutBoxHelper.showAbout()
+          //click: () => dialog.showMessageBox({"message": "Ace Beta", "detail": "DAISY Consortium 2018"})
         }
       );
     }
