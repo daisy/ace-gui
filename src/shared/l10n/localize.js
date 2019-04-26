7acdcf4ec21c99d271bbfce6c66b552bfaf8c276
Daniel Weck <daniel.weck@gmail.com>
2019-04-26 21:16:54 +0100

    concurrent i18next instances

--- src/shared/l10n/localize.js
+++ src/shared/l10n/localize.js
@@ -1,7 +1,9 @@
 import * as i18n from 'i18next';
 
-import * as enJson from "./locales/en.json";
-import * as frJson from "./locales/fr.json";
+// import * as enJson from "./locales/en.json";
+// import * as frJson from "./locales/fr.json";
+const enJson = require("./locales/en.json");
+const frJson = require("./locales/fr.json");
 
 export const LANGUAGES = {
     en: "English",
@@ -11,17 +13,20 @@ export const LANGUAGE_KEYS = Object.keys(LANGUAGES);
 
 export const DEFAULT_LANGUAGE = "en";
 
-// https://www.i18next.com/overview/configuration-options
-i18n.init({
-    debug: false,
-    resources: {
-        en: {
-            translation: enJson.default || enJson,
-        },
-        fr: {
-            translation: frJson.default || frJson,
-        },
+const RESOURCES = {
+    en: {
+        translation: enJson, // enJson.default || enJson,
     },
+    fr: {
+        translation: frJson, // frJson.default || frJson,
+    },
+};
+
+const i18nextInstance = i18n.createInstance();
+// https://www.i18next.com/overview/configuration-options
+i18nextInstance.init({
+    debug: false,
+    resources: RESOURCES,
     // lng: undefined,
     fallbackLng: DEFAULT_LANGUAGE,
     // whitelist: LANGUAGE_KEYS,
@@ -52,9 +57,9 @@ export function setCurrentLanguage(language) {
 export function localize(msg, options) {
     const opts = options || {};
 
-    if (i18n.language !== _currentLanguage) {
-        i18n.changeLanguage(_currentLanguage);
+    if (i18nextInstance.language !== _currentLanguage) {
+        i18nextInstance.changeLanguage(_currentLanguage);
     }
 
-    return i18n.t(msg, opts);
+    return i18nextInstance.t(msg, opts);
 };
