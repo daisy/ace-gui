d822617ee9a887a7504d278cf5c6ffd0c42e4d8c
Daniel Weck <daniel.weck@gmail.com>
2019-07-19 02:24:03 +0100

    working sign + notarize for MacOS

--- build/notarize.js
+++ build/notarize.js
@@ -8,11 +8,11 @@ exports.default = async function notarizing(context) {
   }
 
   const appName = context.packager.appInfo.productFilename;
-
   return await notarize({
     appBundleId: 'org.daisy.ace',
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
     appleIdPassword: process.env.APPLEIDPASS,
+    ascProvider: process.env.APPLEIDTEAM
   });
 };
