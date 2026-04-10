7d9c4b68fbb937d78b7576393aa4690b07c0856b
Daniel Weck <daniel.weck@gmail.com>
2026-04-10 20:19:36 +0100

    chore(build): Apple Notarize with KeyChain credentials store [skip ci]

--- build/notarize.js
+++ build/notarize.js
@@ -1,21 +1,36 @@
 // xcrun notarytool history --apple-id=$APPLEID --team-id=$APPLEIDTEAM --password=$APPLEIDPASS
 
-require('dotenv').config();
-const { notarize } = require('@electron/notarize');
+// xcrun notarytool store-credentials "thorium-keychain" --apple-id $APPLEID --team-id $APPLEIDTEAM --password $APPLEIDPASS
+// export APPLEIDKEYCHAIN="thorium-keychain"
+
+require("dotenv").config();
+const { notarize } = require("@electron/notarize");
 
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;
-  if (electronPlatformName !== 'darwin' || process.env.SKIP_NOTARIZE) {
+  if (electronPlatformName !== "darwin" || process.env.SKIP_NOTARIZE) {
     return;
   }
 
   const appName = context.packager.appInfo.productFilename;
-  return await notarize({
-    appBundleId: 'org.daisy.ace',
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS,
-    teamId: process.env.APPLEIDTEAM,
-    ascProvider: process.env.APPLEIDTEAM
-  });
+
+  // https://github.com/electron/notarize/blob/main/README.md
+  return await notarize(
+    !!process.env.APPLEIDKEYCHAIN
+      ? {
+          appBundleId: "org.edrlab.thorium",
+          appPath: `${appOutDir}/${appName}.app`,
+
+          keychainProfile: process.env.APPLEIDKEYCHAIN,
+        }
+      : {
+          appBundleId: "org.edrlab.thorium",
+          appPath: `${appOutDir}/${appName}.app`,
+
+          appleId: process.env.APPLEID,
+          appleIdPassword: process.env.APPLEIDPASS,
+          teamId: process.env.APPLEIDTEAM,
+          ascProvider: process.env.APPLEIDTEAM, // legacy
+        },
+  );
 };
